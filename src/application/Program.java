14425489bc39801048d1c316190147341d737a98
EduardPortes <epcduas05@hotmail.com>
2024-05-04 21:59:30 -0300

    .

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,45 @@
 package application;
 
 
+import util.Product;
+
 import java.util.Scanner;
 
 public  class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
 
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product prod = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + prod);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        prod.addProducts(sc.nextInt());
+
+
+        System.out.println();
+        System.out.print("Update data: " + prod);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        prod.removeProducts(sc.nextInt());
+
+        System.out.println();
+        System.out.print("Update data: " + prod);
 
 
         sc.close();
---
+++ src/util/Product.java
@@ -0,0 +1,36 @@
+package util;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int p){
+        quantity += p;
+    }
+
+    public void removeProducts(int p){
+        quantity -= p;;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f",price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
