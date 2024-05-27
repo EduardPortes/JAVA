f6f24e6af193f8beb83c187d2b497712f0a94d40
EduardPortes <epcduas05@hotmail.com>
2024-05-27 12:56:59 -0300

    ChessException and ChessPosition

---
+++ src/chess/chess/ChessPosition.java
@@ -0,0 +1,40 @@
+package chess.chess;
+
+import chess.boardgame.Position;
+
+import java.util.IllegalFormatCodePointException;
+
+public class ChessPosition {
+
+    private char column;
+    private  int row;
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new ChessException("Error instantiating ChessPositon. Valid values are from a1 to h8");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){
+        return new Position(8 - row,  column - 'a');
+    }
+
+    protected static ChessPosition fromPostion(Position position){
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getColumn());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
