395f61bab5683c3810931660182232da7a6f8b0b
EduardPortes <epcduas05@hotmail.com>
2024-08-07 17:30:48 -0300

    Testing new things

--- src/progstudo/ztestes/application/Program.java
+++ src/progstudo/ztestes/application/Program.java
@@ -1,50 +1,48 @@
 package progstudo.ztestes.application;
 
-import progstudo.ztestes.entities.Product;
-import progstudo.ztestes.services.CalculationService;
+import progstudo.ztestes.entities.LogEntry;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Program {
     public static void main(String[] args) {
 
-        List<Product> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        String path = "D:\\Downloads\\in.txt";
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
 
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
 
+            Set<LogEntry> set = new HashSet<>();
+
             String line = br.readLine();
-            while (line != null){
-                String[] pd = line.split(",");
-                String name = pd[0];
-                Double price = Double.parseDouble(pd[1]);
+            while(line != null){
 
-                Product p = new Product(name, price);
-                list.add(p);
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
                 line = br.readLine();
-            }
 
-            Product x = CalculationService.max(list);
-            System.out.println("Max: ");
-            System.out.println(x);
+            }
+            System.out.println("Total users: " + set.size());
+
 
         } catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error " + e.getMessage());
         }
 
-
-
-
-
-
-
-
-
+        sc.close();
     }
 
 }
